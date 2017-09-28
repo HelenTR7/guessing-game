9668eb51be9dc00b50c1fbb37520896ead5ffb93
Helen Trapeznikova <31996958+HelenTR7@users.noreply.github.com>
2017-09-29 02:21:45 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,37 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor(start=0, finish=0) { 
+        this.start=start;
+        this.finish=finish;
+    }
 
     setRange(min, max) {
+      this.start=min;
+      this.finish=max;
 
     }
 
     guess() {
-
+      var solut=0;
+      var i=this.start;
+      var j=this.finish;
+      solut=Math.ceil((i+j)/2);
+      return solut; 
     }
 
     lower() {
 
+      var i=this.start;
+      var j=this.finish;    
+      this.finish=Math.ceil((i+j)/2);
+ 
     }
 
     greater() {
-
+      var i=this.start;
+      var j=this.finish;    
+      this.start=Math.ceil((i+j)/2);
+ 
     }
 }
-
 module.exports = GuessingGame;
